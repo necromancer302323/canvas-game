3b0c48a4efa5182a1b7bd532152a63876c0af6df
Aaryan Singh <aaryansingh@Aaryans-Mini.lan>
2025-02-17 21:20:01 +0530

    tried to fix a bug

--- src/main.js
+++ src/main.js
@@ -41,123 +41,122 @@ socket.onopen = function () {
 };
 socket.onmessage = function (message) {
   const data = JSON.parse(message.data);
-  console.log(data)
+  console.log(data);
   if (data.event === "acknowledged") {
-    addCurrentUserPlayer( data.data.userId,data.data.userPosition );
+    addCurrentUserPlayer(data.data.userId, data.data.userPosition);
     if (data.playerList != []) {
       data.playerList.forEach((users) => {
-        if(users.id!=data.data.userId){
-        playersList.push({
-          userId: users.id,
-          player: new Sprite({
-            resource: resources.images.hero,
-            frameSize: new Vector2(32, 32),
-            hFrames: 3,
-            vFrames: 8,
-            frame: 1,
-          }),
-          userPosition:data.userPosition
-        });
-    }});
+        if (users.id != data.data.userId) {
+          playersList.push({
+            userId: users.id,
+            player: new Sprite({
+              resource: resources.images.hero,
+              frameSize: new Vector2(32, 32),
+              hFrames: 3,
+              vFrames: 8,
+              frame: 1,
+            }),
+            userPosition: data.userPosition,
+          });
+        }
+      });
     }
   }
-  if(data.event=="position-update"){
-    function findingWhichPlayerMoved(user){
-      return user.userId==data.data.userId
+  if (data.event == "position-update") {
+    function findingWhichPlayerMoved(user) {
+      return user.userId == data.data.userId;
     }
-   const movedPlayer=playersList.findIndex(findingWhichPlayerMoved)
-   playersList[movedPlayer]
+    const movedPlayer = playersList.findIndex(findingWhichPlayerMoved);
+    playersList[movedPlayer];
   }
-  if(data.event=="User added"){
+  if (data.event == "User added") {
     playersList.push({
       userId: data.userId,
-          player: new Sprite({
-            resource: resources.images.hero,
-            frameSize: new Vector2(32, 32),
-            hFrames: 3,
-            vFrames: 8,
-            frame: 1,
-          }),
-          userPosition:data.userPosition
-    })
+      player: new Sprite({
+        resource: resources.images.hero,
+        frameSize: new Vector2(32, 32),
+        hFrames: 3,
+        vFrames: 8,
+        frame: 1,
+      }),
+      userPosition: data.userPosition,
+    });
   }
-  if(data.event=== "userLeft"){
-   const playersLeft= playersList.findIndex(findingPLayerLeft)
-   function findingPLayerLeft(user){
-    return user.userId==data.userId
-   }
-   if(playersLeft==-1){
-    playersList.pop()
-   }else{
-    playersList.splice(playersLeft,1)
-   }
+  if (data.event === "userLeft") {
+    const playersLeft = playersList.findIndex(findingPLayerLeft);
+    function findingPLayerLeft(user) {
+      return user.userId == data.userId;
+    }
+    if (playersLeft == -1) {
+      playersList.pop();
+    } else {
+      playersList.splice(playersLeft, 1);
+    }
   }
 
-function update() {
-  const positionData = {
-    event: "position",
-    data: {
-      x: heroPos.x,
-      y: heroPos.y,
-    },
-  };
-  if (input.direction === DOWN) {
-    heroPos.y += 1;
-    socket.send(JSON.stringify(positionData));
+  function update() {
+    const positionData = {
+      event: "position",
+      data: {
+        x: heroPos.x,
+        y: heroPos.y,
+      },
+    };
+    if (input.direction === DOWN) {
+      heroPos.y += 1;
+      socket.send(JSON.stringify(positionData));
+    }
+    if (input.direction === UP) {
+      heroPos.y -= 1;
+      socket.send(JSON.stringify(positionData));
+    }
+    if (input.direction === LEFT) {
+      heroPos.x -= 1;
+      socket.send(JSON.stringify(positionData));
+    }
+    if (input.direction === RIGHT) {
+      heroPos.x += 1;
+      socket.send(JSON.stringify(positionData));
+    }
   }
-  if (input.direction === UP) {
-    heroPos.y -= 1;
-    socket.send(JSON.stringify(positionData));
-  }
-  if (input.direction === LEFT) {
-    heroPos.x -= 1;
-    socket.send(JSON.stringify(positionData));
-  }
-  if (input.direction === RIGHT) {
-    heroPos.x += 1;
-    socket.send(JSON.stringify(positionData));
-  }
-}
 
-function draw() {
-  skySprite.drawImage(ctx, 0, 0);
-  groundSprite.drawImage(ctx, 0, 0);
-  const heroOffset = new Vector2(-8, -21);
-  const heroPosX = heroPos.x + heroOffset.x;
-  const heroPosY = heroPos.y + heroOffset.y;
-  if (hero != null) {
-    hero.character.drawImage(ctx, heroPosX, heroPosY);
+  function draw() {
+    skySprite.drawImage(ctx, 0, 0);
+    groundSprite.drawImage(ctx, 0, 0);
+    const heroOffset = new Vector2(-8, -21);
+    const heroPosX = heroPos.x + heroOffset.x;
+    const heroPosY = heroPos.y + heroOffset.y;
+    if (hero != null) {
+      hero.character.drawImage(ctx, heroPosX, heroPosY);
+    }
+    if (playersList && playersList != [] && playersList[1] != undefined) {
+      playersList.forEach((user) => {
+        if (playersList.player != hero.character) {
+          user.player.drawImage(ctx, user.userPosition.x, user.userPosition.y);
+        }
+      });
+    }
+    shadow.drawImage(ctx, heroPosX, heroPosY);
   }
- if(playersList&&playersList!=[]&&playersList[1]!=undefined){
-  playersList.forEach((user)=>{
-  
-    user.player.drawImage(ctx,user.userPosition.x,user.userPosition.y)
-  })
- }
-  shadow.drawImage(ctx, heroPosX, heroPosY);
-}
-function addCurrentUserPlayer(userId,userPosition) {
-  hero = {
-    character: new Sprite({
-      resource: resources.images.hero,
-      frameSize: new Vector2(32, 32),
-      hFrames: 3,
-      vFrames: 8,
-      frame: 1,
-    }),
-    userId: userId,
-  };
-  playersList.push({ userId, player: hero.character,userPosition });
-}
+  function addCurrentUserPlayer(userId, userPosition) {
+    hero = {
+      character: new Sprite({
+        resource: resources.images.hero,
+        frameSize: new Vector2(32, 32),
+        hFrames: 3,
+        vFrames: 8,
+        frame: 1,
+      }),
+      userId: userId,
+    };
+    playersList.push({ userId, player: hero.character, userPosition });
+  }
 
-const gameLoop = new GameLoop(update, draw);
-gameLoop.start();
-
+  const gameLoop = new GameLoop(update, draw);
+  gameLoop.start();
 };
-socket.onclose = function (){
-  playersList.filter((user)=>{
-    user.userId!=data.userId
-  })
-}
-
-
+socket.onclose = function () {
+  playersList.filter((user) => {
+    user.userId != data.userId;
+  });
+};
